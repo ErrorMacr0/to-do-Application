d3f215de83cbf6f9a0bc6be401608d2a66d93b7c
students <students@stepdn.com>
2019-11-27 18:36:26 +0300

    updated script

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 "use strict"
 
+window.addEventListener('DOMContentLoaded', () => {
+    const task = [
+        {
+            id: '1',
+            title: 'New task',
+            body: 'Some text'
+        },
+        {
+            id: '2',
+            title: 'New task 2',
+            body: 'Some text'
+        },
+        {
+            id: '3',
+            title: 'New task 3',
+            body: 'Some text'
+        },
+        {
+            id: '4',
+            title: 'New task 4',
+            body: 'Some text'
+        }
+    ]
+
+    const form = document.forms['taskForm'];
+
+    const inputTitle = form[0];
+    const inputBody = form[1];
+
+    form.addEventListener('submit', onFormSubmit);
+
+    function onFormSubmit( e ) {
+        e.preventDefault();
+    }
+
+});
